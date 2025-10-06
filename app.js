1e328b629bf38c84e409932f8ac7edd5cdf0b162
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-10-06 13:22:57 -0500

    feat: initial commit for app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+document.getElementById('start-btn').addEventListener('click', function() {
+    const location = document.getElementById('location-input').value;
+    if (location) {
+        startARNavigation(location);
+    } else {
+        alert('Bitte geben Sie einen Standort ein.');
+    }
+});
+
+function startARNavigation(location) {
+    console.log('Starte AR-Navigation für:', location);
+    // Hier könnte der Code zum Starten der Augmented-Reality-Navigation implementiert werden.
+}
